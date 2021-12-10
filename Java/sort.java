9d87fceca6464f4938cc76cf6a0cdc0f9cd3c5e1
DevTenga <rebloxemailtenga2341@gmail.com>
2021-12-10 16:05:09 +0530

    Devised an algorithm for sorting strings (using long data types).

--- Java/sort.java
+++ Java/sort.java
@@ -6,6 +6,16 @@ import java.lang.Math;
 
 class sort {
 	static class Helper {
+
+		// Check for the largest string in a string array.
+		int getLargestStringLen(String[] strArr) {
+			int largest = 0;
+			for (String str: strArr) {
+				int len = str.length();
+				if (len > largest) largest = len; 
+			}
+			return largest;
+		}
 		
 		// Check if character is valid and retrun ascii value. Exit from system for invalid characters.
 		int getAsciiEquiv(char ch) {
@@ -16,27 +26,34 @@ class sort {
 			} else if (val >= 97 && val <= 122) {
 				val -= 96;
 			} else {
-				System.out.println("\u001B[31mError! Entered string contains an invalid character. Please enter characters within range: (A-Z) (a-z)\u001B[0m");
+				System.out.println("\u001B[31mError! Entered string contains an invalid character. Please enter characters within range: (A-Z) (a-z)\u001B[0m\n");
 				System.exit(1);
 			}
 			return val; 
 		};
 
-
-		int[] getAsciiArr(String[] strArr, int base) {
-			int[] asciiArr = new int[strArr.length];
+		// Get an integer table equivalent to the string table.
+		long[] getAsciiArr(String[] strArr, int base) {
+			int largest = getLargestStringLen(strArr);
+			long[] asciiArr = new long[strArr.length];
 
 			for (int i = 0; i <= strArr.length - 1; i++) {
 				String element = strArr[i];
-				int val = 0;
+				int len = element.length();
+				long val = 0;
 
-				for (int j = 0; j <= element.length() - 1; j++) {
-					val += Math.pow(base,j) * getAsciiEquiv(element.charAt(j));
+				for (int j = 0; j <= len - 1; j++) {
+					val += Math.pow(base,largest-j-1) * getAsciiEquiv(element.charAt(j));
+				}
+
+				if (val >= Long.MAX_VALUE) {
+					System.out.println("\u001B[31mError! One of the entered strings is too large. Please reduce the length of the largest string and retry.\u001B[0m\n");
+					System.exit(1);
 				}
 				asciiArr[i] = val;
 			}
 
-			return asciiArr ;
+			return asciiArr;
 		};
 
 	}
@@ -44,7 +61,7 @@ class sort {
 
 	public static void main(String[] args) {
 		Helper helper = new Helper();
-		for (int i: helper.getAsciiArr(args,100)) {
+		for (long i: helper.getAsciiArr(args,100)) {
 			System.out.printf("%d\n",i);
 		}
 	}
