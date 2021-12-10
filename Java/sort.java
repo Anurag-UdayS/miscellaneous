3931b5f84afdcd2c07f03d04ce4eb15183d38ebe
DevTenga <rebloxemailtenga2341@gmail.com>
2021-12-10 16:05:09 +0530

    Devised an algorithm for sorting strings (using long data types).

--- Java/sort.java
+++ Java/sort.java
@@ -5,6 +5,22 @@
 import java.lang.Math;
 
 class sort {
+
+	// Stores the max, min values and indices.
+	static class values {
+		long max,min;
+		int idx_max,idx_min;
+
+		public values(long max, long min, int idx_max, int idk_min) {
+			this.max = max;
+			this.min = min;
+			this.idx_max = idx_max;
+			this.idx_min = idk_min;
+		}
+
+	}
+
+
 	static class Helper {
 
 		// Check for the largest string in a string array.
@@ -26,7 +42,7 @@ class sort {
 			} else if (val >= 97 && val <= 122) {
 				val -= 96;
 			} else {
-				System.out.println("\u001B[31mError! Entered string contains an invalid character. Please enter characters within range: (A-Z) (a-z)\u001B[0m\n");
+				System.out.println("\u001B[31mError! Entered string contains an invalid character. Please enter characters within range: (A-Z) (a-z)\u001B[0m");
 				System.exit(1);
 			}
 			return val; 
@@ -47,7 +63,7 @@ class sort {
 				}
 
 				if (val >= Long.MAX_VALUE) {
-					System.out.println("\u001B[31mError! One of the entered strings is too large. Please reduce the length of the largest string and retry.\u001B[0m\n");
+					System.out.println("\u001B[31mError! One of the entered strings is too large. Please reduce the length of the largest string and retry.\u001B[0m");
 					System.exit(1);
 				}
 				asciiArr[i] = val;
@@ -55,6 +71,32 @@ class sort {
 
 			return asciiArr;
 		};
+
+
+		// Get the maximum and minimum values.
+		values getMaxAndMin(long[] asciiArr, int lastIdx) {
+			long max = 0;
+			long min = Long.MAX_VALUE;
+			int idx_max = 0;
+			int idx_min = Int.MAX_VALUE;
+
+			for (i = 0; i <= lastIdx; i++) {
+				long element = asciiArr[i];
+				if (element > max) {
+					max = element;
+					idx_max = i;
+				} else if (element < min) {
+					min = element;
+					idx_min = i;
+				}
+			};
+
+			return new values(max, min, idx_max, idx_min);
+		}
+
+
+		// Sort long table in an ascending order.
+
 
 	}
 
