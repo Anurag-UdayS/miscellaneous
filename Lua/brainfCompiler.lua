44291fd5c164e79fe171c81bc4fe9d3a45f7fa66
Anurag Tewary <anuragtewary@outlook.com>
2021-12-13 16:37:02 +0530

    Brainf.lua modified

--- Lua/brainfCompiler.lua
+++ Lua/brainfCompiler.lua
@@ -25,8 +25,9 @@ function pointer:MoveRight()
 
 	if idx == 30000 then
 		error("Index 30,001 out of bounds.")
+	else
+		self.currentIndex = idx + 1
 	end
-	self.currentIndex = idx + 1
 end
 
 function pointer:MoveLeft()
@@ -34,22 +35,38 @@ function pointer:MoveLeft()
 
 	if idx == 0 then
 		error("Index -1 out of bounds.")
+	else
+		self.currentIndex = idx - 1
 	end
-	self.currentIndex = idx - 1
 end
 
 function pointer:Increment()
 	local idx = self.currentIndex
-	main[idx] = main[idx] + 1
+	
+	if main[idx] then
+		main[idx] = main[idx] + 1
+	else
+		main[idx] = 1
+	end
 end
 
 function pointer:Decrement()
 	local idx = self.currentIndex
-	main[idx] = main[idx] - 1
+
+	if main[idx] then
+		main[idx] = main[idx] - 1
+	else
+		main[idx] = -1
+	end
+
 end
 
-function function_name( ... )
-	-- body
+function pointer:Output()
+	io.write(string.char(main[self.currentIndex]))
+end
+
+function pointer:TakeInput()
+	main[self.currentIndex] = string.byte(io.read(1))
 end
 
 local config = {
@@ -57,4 +74,21 @@ local config = {
 	["<"] = function() pointer:MoveLeft() end,
 	["+"] = function() pointer:Increment() end,
 	["-"] = function() pointer:Decrement() end,
+	["."] = function() pointer:Output() end,
+	[","] = function() pointer:TakeInput() end,
 }
+
+function main( ... )
+	local data,message = io.open(arg[1],"r")
+	
+	if not data then
+		_error("Cannot open file. "..message)
+	end
+
+	for i = 1,#data do
+		local element = string.sub(data,i,i)
+		
+	end
+
+
+end
