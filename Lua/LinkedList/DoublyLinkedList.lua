7dc323971b0add10de1723b9e2e7cbb0f1f6e61c
Anurag Tewary <anuragtewarry772@gmail.com>
2022-04-26 16:38:29 +0530

    Added test cases for Lua LinkedList

---
+++ Lua/LinkedList/DoublyLinkedList.lua
@@ -0,0 +1,335 @@
+#!/usr/bin/lua
+
+-- Doubly Linked List
+-- Anurag Tewary
+-- 24th April, 2022
+--
+-- An implementation of the DoublyLinkedList data structure, written in Lua.
+-- Demonstrates OOP programming structures.
+
+-- Abstract structure of the DoublyLinkedList
+local DoublyLinkedList = {Node = {__object = {}, __meta = {}}, __object = {}, __meta = {}};
+
+-- Color raised errors red.
+local _error = error;
+local _assert = assert;
+
+function error(msg)
+	_error("\027[31m" .. msg .. "\027[0m")
+end
+
+function assert(cond, msg)
+	_assert(cond, "\027[31m" .. msg .. "\027[0m")
+end
+
+DoublyLinkedList = {
+	-- The Node class
+	Node = {			
+		-- Metadata for Instance Method container.
+		__meta = {
+			--__metatable = "Container of Instance Methods for DoublyLinkedList.Node",
+			__newindex = function(self, k, v) 
+				if (k ~= "prev" and k ~= "next" and k ~= "value") then 
+					error("Instance Table. Cannot be modified. (unknown key: '" .. k .. "')"); 
+				else
+					rawset(self, k, v)
+				end; 
+			end,
+
+			__tostring = function(self) return tostring(self.value); end,
+			__index = DoublyLinkedList.Node.__object, 
+			__type = "<class 'DoublyLinkedList/Node'>"
+
+		},
+
+		-- Instance methods will be stored here.
+		__object = DoublyLinkedList.Node.__object -- Reference to the older table is saved.
+
+	},
+
+
+	-- Metadata for Instance Method container
+	__meta = {
+		__metatable = "Container of Instance Methods for DoublyLinkedList",
+		__index = DoublyLinkedList.__object,
+		__type = "<class 'DoublyLinkedList'>",
+		__newindex = function(self, k, v) 
+			if (k ~= "head" and k ~= "tail") then 
+				error("Instance Table. Cannot be modified. (unknown key: '" .. k .. "')"); 
+			else
+				rawset(self, k, v)	
+			end; 
+		end,
+		__tostring = function(self) 
+			if (not self.head) then return "[]" end
+			local str = "["
+			local current = self.head
+
+			while (current) do
+				if (current.next) then
+					str = str .. current:tostring() .. ", ";
+					current = current.next;
+				else
+					return str .. current:tostring() .. "]";
+				end
+			end
+
+		end
+	},
+	
+	-- Instance methods will be stored here.
+	__object = DoublyLinkedList.__object -- Reference to the older table is saved.
+}
+
+--====================================================================================================--
+--								DoublyLinkedList.Node --> Instance Methods
+--====================================================================================================--
+-- Instance methods of DoublyLinkedList's Node class.
+-- Methods are written outside in order to utilize eye candy.
+
+function DoublyLinkedList.Node.__object.getClass()
+	return DoublyLinkedList.Node;
+end
+
+function DoublyLinkedList.Node.__object:tostring() 
+	return tostring(self); 
+end
+
+--====================================================================================================--
+--								DoublyLinkedList.Node --> Class Methods
+--====================================================================================================--
+-- Class methods of DoublyLinkedList's Node.
+-- Includes constructor, hardcoded class methods and utility functions.
+
+function DoublyLinkedList.Node.new(val, next, prev)
+	assert(DoublyLinkedList.Node.isNode(next) or next == nil, "<next> must be a Node.")
+	assert(DoublyLinkedList.Node.isNode(prev) or prev == nil, "<prev> must be a Node.")
+	return setmetatable({value = val, next = next, prev = prev}, DoublyLinkedList.Node.__meta)
+end
+
+-- Get simple name of the class.
+function DoublyLinkedList.Node.getSimpleName()
+	return "Node"
+end
+
+-- Get full name of the class.
+function DoublyLinkedList.Node.getFullName()
+	return "DoublyLinkedList/Node"
+end
+
+-- Check if passed arg is instance of Node.
+-- Simple implementation of `instanceof`
+function DoublyLinkedList.Node.isNode(node)
+	return type(node) == "<class 'DoublyLinkedList/Node'>";
+end
+
+
+--====================================================================================================--
+--								DoublyLinkedList --> Instance Methods
+--====================================================================================================--
+-- Instance Methods of DoublyLinkedList.
+-- All implementations of the LinkedList API will be demonstrated here.
+
+
+function DoublyLinkedList.__object.getClass()
+	return DoublyLinkedList;
+end
+
+function DoublyLinkedList.__object:tostring() 
+	return tostring(self); 
+end
+
+-- Reverse tostring for traversing from tail.
+function DoublyLinkedList.__object:tostringTailToHead() 
+	if (not self.tail) then return "[]" end
+	local str = "["
+	local current = self.tail
+
+	while (current) do
+		if (current.prev) then
+			str = str .. current:tostring() .. ", ";
+			current = current.prev;
+		else
+			return str .. current:tostring() .. "]";
+		end
+	end
+
+end
+
+-- Helper function for `DoublyLinkedList#addAt`
+local function addAt(current, idx, node, lastNode, self)
+	if (current == idx) then
+		lastNode.prev.next = node
+		node.prev = lastNode.prev
+		lastNode.prev = node
+		node.next = lastNode
+	else
+		addAt(current + 1, idx, node, lastNode.next, self)	
+	end
+end
+
+
+function DoublyLinkedList.__object:addAt(idx, node)
+	if (idx == 1) then 
+		self:addFirst(node)
+		return;
+	end
+
+	size = self:size()
+
+	if (idx > size + 1 or idx < 1) then
+		error("Index (" .. tostring(idx) .. ") is out of bounds.")
+	end
+
+	if (idx == size + 1) then
+		self:addLast(node)
+		return;
+	end
+
+	if (not DoublyLinkedList.Node.isNode(node)) then
+		node = DoublyLinkedList.Node.new(node)
+	end
+
+
+	addAt(1, idx, node, self.head, self)
+end
+
+function DoublyLinkedList.__object:addAll(t)
+	if (not next(t)) then return; end
+	assert(type(t) == 'table' and t[1] ~= nil, "Table must be an array (indexed with 1).")
+	
+	for _, node in ipairs(t) do 
+		if (not DoublyLinkedList.Node.isNode(node)) then
+			node = DoublyLinkedList.Node.new(node)
+		end
+		self:addLast(node) 
+	end
+
+end
+
+
+function DoublyLinkedList.__object:addFirst(node)
+	if (not DoublyLinkedList.Node.isNode(node)) then
+		node = DoublyLinkedList.Node.new(node)
+	end
+
+	if (self.head) then
+		self.head.prev = node
+		node.next = self.head
+		node.prev = nil
+	end
+
+	self.head = node
+	if (not self.tail) then self.tail = node end
+end
+
+DoublyLinkedList.__object.offerFirst 	= DoublyLinkedList.__object.addFirst
+DoublyLinkedList.__object.offerLast 	= DoublyLinkedList.__object.addLast
+DoublyLinkedList.__object.offerLast 	= DoublyLinkedList.__object.addLast
+DoublyLinkedList.__object.offerLast 	= DoublyLinkedList.__object.addLast
+
+
+function DoublyLinkedList.__object:addLast(node)
+	if (not DoublyLinkedList.Node.isNode(node)) then
+		node = DoublyLinkedList.Node.new(node)
+	end
+
+	if (self.tail) then
+		self.tail.next = node
+		node.prev = self.tail
+		node.next = nil
+	end
+	
+	self.tail = node
+	if (not self.head) then self.head = node end
+end
+
+DoublyLinkedList.__object.add 		= DoublyLinkedList.__object.addLast
+DoublyLinkedList.__object.offer 	= DoublyLinkedList.__object.addLast
+DoublyLinkedList.__object.offerLast = DoublyLinkedList.__object.addLast
+DoublyLinkedList.__object.push 		= DoublyLinkedList.__object.addLast
+
+
+function DoublyLinkedList.__object:size()
+	local current = self.head
+	local size = 0
+	while (current) do
+		size = size + 1
+		current = current.next
+	end
+	return size;
+end
+
+--====================================================================================================--
+--								DoublyLinkedList --> Class Methods
+--====================================================================================================--
+-- Class methods of DoublyLinkedList.
+-- Includes constructors and hardcoded class methods.
+
+function DoublyLinkedList.new(head, tail)
+	if (head ~= nil and not DoublyLinkedList.Node.isNode(head)) then
+		head = DoublyLinkedList.Node.new(head);
+	end
+
+	if (tail ~= nil and not DoublyLinkedList.Node.isNode(tail)) then
+		tail = DoublyLinkedList.Node.new(tail);
+		tail.prev = head
+	elseif (tail == nil) then
+		tail = head
+	end
+
+	if (head and head ~= tail) then head.next = tail end
+
+	return setmetatable({head = head, tail = tail}, DoublyLinkedList.__meta);
+end	
+	
+function DoublyLinkedList.fromTable(t)
+	if (not next(t)) then return DoublyLinkedList.new(); end
+	assert(type(t) == 'table' and t[1] ~= nil, "Table must be an array (indexed with 1).")
+	
+	local list = DoublyLinkedList.new();
+
+	local current;
+
+	for _, val in ipairs(t) do
+		local node = DoublyLinkedList.Node.new(val)
+
+		if (not list.head) then
+			list.head = node
+		else
+			node.prev = current
+			current.next = node
+		end
+			
+		current = node
+	end
+
+	list.tail = current
+	return list
+end
+
+
+-- Get simple name of the class.
+function DoublyLinkedList.getSimpleName()
+	return "DoublyLinkedList"
+end
+
+-- Get full name of the class.
+function DoublyLinkedList.getFullName()
+	return "DoublyLinkedList"
+end
+--====================================================================================================--
+--====================================================================================================--
+
+
+-- Protecting the Class tables.
+local protected = {}
+
+function protected.__newindex(_, k)
+	error("Class Table. Cannot be modified. (unknown key: '" .. k .. "')"); 
+end
+
+setmetatable(DoublyLinkedList, protected)
+setmetatable(DoublyLinkedList.Node, protected)
+	
+return DoublyLinkedList;
---
+++ Lua/LinkedList/Test.lua
@@ -0,0 +1,276 @@
+#!/usr/bin/lua
+
+-- Test file
+-- Anurag Tewary
+-- 24th April, 2022
+--
+-- The file used to test the DoublyLinkedList implementation.
+
+-- assertEquals implementation
+function assertEquals(b1, b2, testcase)
+	if (b1 == b2) then
+		print("\027[1;32mTest " .. testcase .. " was successful!\027[0m")
+	else
+		error("\027[31mTest " .. testcase .. " failed!\027[0m\n"
+			.. "\027[33mExpected:\027[0m " .. b2 .. "\n" 
+			.. "\027[33mReceived:\027[0m " .. b1 .. "\n")
+	end
+end
+
+local DoublyLinkedList = require 'DoublyLinkedList'
+local dll -- our object
+
+-- Test 0.0.0
+-- Construction of a new DoublyLinkedList (without parameters).
+dll = DoublyLinkedList.new()
+assertEquals(dll:tostring(), "[]", "0.0.0.1")
+assertEquals(dll:tostringTailToHead(), "[]", "0.0.0.2")
+
+-- Test 0.1.0
+-- Testing the pointed class.
+dll = DoublyLinkedList.new()
+assertEquals(dll.getClass(), DoublyLinkedList, "0.1.0.0")
+assertEquals(dll.getClass().getSimpleName(), "DoublyLinkedList", "0.1.0.1")
+assertEquals(dll.getClass().getFullName(), "DoublyLinkedList", "0.1.0.2")
+
+-- Test 0.1.1
+-- Testing the pointed node class
+dll = DoublyLinkedList.new(0)
+assertEquals(dll.head.getClass(), DoublyLinkedList.Node, "0.1.1.0")
+assertEquals(dll.head.getClass().getSimpleName(), "Node", "0.1.1.1")
+assertEquals(dll.head.getClass().getFullName(), "DoublyLinkedList/Node", "0.1.1.2")
+
+-- Test 0.2.0
+-- Adding an unknown key to the protected classes.
+local testcase;
+dll = DoublyLinkedList.new(5)
+
+local function classError(err)
+	if (err:find("failed!")) then
+		print(err)
+	else
+		err = err:sub(err:find('\027%[31m') + 5, #err - 4) -- Picking out the message and removing the control characters.
+		assertEquals(err, "Class Table. Cannot be modified. (unknown key: 'foo')", testcase)
+	end
+end
+
+testcase = "0.2.0.0"
+xpcall(function() DoublyLinkedList.foo = "bar"; end, classError)
+
+testcase = "0.2.0.1"
+xpcall(function() DoublyLinkedList.Node.foo = "bar"; end, classError)
+
+-- Test 0.3.0
+-- Adding an unknown key to the protected instances.
+local function instanceError(err)
+	if (err:find("failed!")) then
+		print(err)
+	else
+		err = err:sub(err:find('\027%[31m') + 5, #err - 4) -- Picking out the message and removing the control characters.
+		assertEquals(err, "Instance Table. Cannot be modified. (unknown key: 'foo')", testcase)
+	end
+end
+
+testcase = "0.3.0.0"
+xpcall(function() DoublyLinkedList.new().foo = "bar"; end, instanceError)
+
+testcase = "0.3.0.1"
+xpcall(function() DoublyLinkedList.Node.new().foo = "bar"; end, instanceError)
+
+testcase = nil
+
+-- Test 1.0.0
+-- Construction of a new DoublyLinkedList (with just head as a parameter).
+dll = DoublyLinkedList.new(5)
+assertEquals(dll:tostring(), "[5]", "1.0.1.1")
+assertEquals(dll:tostringTailToHead(), "[5]", "1.0.1.2")
+
+-- Test 1.0.1
+-- Construction of a new DoublyLinkedList (with a head and a tail as parameters.)
+dll = DoublyLinkedList.new(1,5)
+assertEquals(dll:tostring(), "[1, 5]", "1.0.1.1")
+assertEquals(dll:tostringTailToHead(), "[5, 1]", "1.0.1.2")
+
+-- Test 1.1.0
+-- Adding two values to the head of an empty list.
+dll = DoublyLinkedList.new()
+dll:addFirst(4)
+dll:addFirst(3)
+assertEquals(dll:tostring(), "[3, 4]", "1.1.0.1")
+assertEquals(dll:tostringTailToHead(), "[4, 3]", "1.1.0.2")
+
+-- Test 1.1.1
+-- Adding two values to the head of a head-only list.
+dll = DoublyLinkedList.new(5)
+dll:addFirst(4)
+dll:addFirst(3)
+assertEquals(dll:tostring(), "[3, 4, 5]", "1.1.1.1")
+assertEquals(dll:tostringTailToHead(), "[5, 4, 3]", "1.1.1.2")
+
+-- Test 1.1.2
+-- Adding two values to the head of a head and tail list.
+dll = DoublyLinkedList.new(1,5)
+dll:addFirst(4)
+dll:addFirst(3)
+assertEquals(dll:tostring(), "[3, 4, 1, 5]", "1.1.2.1")
+assertEquals(dll:tostringTailToHead(), "[5, 1, 4, 3]", "1.1.2.2")
+
+-- Test 1.2.0
+-- Adding two values to the tail of an empty list.
+dll = DoublyLinkedList.new()
+dll:addLast(4)
+dll:addLast(3)
+assertEquals(dll:tostring(), "[4, 3]", "1.2.0.1")
+assertEquals(dll:tostringTailToHead(), "[3, 4]", "1.2.0.2")
+
+
+-- Test 1.2.1
+-- Adding two values to the tail of a head-only list.
+dll = DoublyLinkedList.new(5)
+dll:addLast(4)
+dll:addLast(3)
+assertEquals(dll:tostring(), "[5, 4, 3]", "1.2.1.1")
+assertEquals(dll:tostringTailToHead(), "[3, 4, 5]", "1.2.1.2")
+
+-- Test 1.2.2
+-- Adding two values to the head of a head and tail list.
+dll = DoublyLinkedList.new(1,5)
+dll:addLast(6)
+dll:addLast(7)
+assertEquals(dll:tostring(), "[1, 5, 6, 7]", "1.2.2.1")
+assertEquals(dll:tostringTailToHead(), "[7, 6, 5, 1]", "1.2.2.2")
+
+-- Test 1.2.3
+-- Adding values in both ways of a head and tail list.
+dll = DoublyLinkedList.new(5,10)
+dll:addLast(11)
+dll:addLast(12)
+dll:addFirst(4)
+dll:addFirst(3)
+dll:addLast(13)
+dll:addFirst(2)
+assertEquals(dll:tostring(), "[2, 3, 4, 5, 10, 11, 12, 13]", "1.2.3.1")
+assertEquals(dll:tostringTailToHead(), "[13, 12, 11, 10, 5, 4, 3, 2]", "1.2.3.2")
+
+-- NOTE: Due to similarity of behavior of `new` and `fromTable` constructors,
+-- Rest of the test cases will be written with tables constructed by `fromTable` constructor.
+
+-- Test 2.0.0
+-- Creating an empty list using `fromTable`.
+dll = DoublyLinkedList.fromTable {}
+assertEquals(dll:tostring(), "[]", "2.0.0.1")
+assertEquals(dll:tostringTailToHead(), "[]", "2.0.0.2")
+
+-- Test 2.0.1
+-- Creating a list with values using `fromTable`.
+dll = DoublyLinkedList.fromTable {1,2,3,4,5}
+assertEquals(dll:tostring(), "[1, 2, 3, 4, 5]", "2.0.1.1")
+assertEquals(dll:tostringTailToHead(), "[5, 4, 3, 2, 1]", "2.0.1.2")
+
+-- Test 2.0.2
+-- Creating a list with map.
+xpcall(function()
+		dll = DoublyLinkedList.fromTable {a = 1}
+	end,
+	function()
+		if (err:find("failed!")) then
+			print(err)
+		else
+			err = err:sub(err:find('\027%[31m') + 5, #err - 4) -- Picking out the message and removing the control characters.
+			assertEquals(err, "Table must be an array (indexed with 1).", "2.0.2.0")
+		end
+	end
+)
+
+
+-- Helper function for Tests in 2.1
+local testcase;
+local function addError(err)
+	if (err:find("failed!")) then
+		print(err)
+	else
+		err = err:sub(err:find('\027%[31m') + 5, #err - 4) -- Picking out the message and removing the control characters.
+		assertEquals(err, "Index (5) is out of bounds.", testcase)
+	end
+end
+
+-- Test 2.1.0
+-- `DoublyLinkedList#addAt(1)` with empty list.
+dll = DoublyLinkedList.fromTable {}
+dll:addAt(1, "foo")
+assertEquals(dll:tostring(), "[foo]", "2.1.0.0")
+assertEquals(dll:tostringTailToHead(), "[foo]", "2.1.0.1")
+
+-- Test 2.1.1
+-- `DoublyLinkedList#addAt(5)` with empty list.
+testcase = "2.1.1.0"
+dll = DoublyLinkedList.fromTable {}
+xpcall (function() dll:addAt(5, "foo") end, addError) 
+
+-- Test 2.1.2 
+-- `DoublyLinkedList#addAt(1)` with list having one value.
+dll = DoublyLinkedList.fromTable {"bar"}
+dll:addAt(1, "foo")
+assertEquals(dll:tostring(), "[foo, bar]", "2.1.2.0")
+assertEquals(dll:tostringTailToHead(), "[bar, foo]", "2.1.2.1")
+
+-- Test 2.1.3
+-- `DoublyLinkedList#addAt(2)` with list having one value.
+dll = DoublyLinkedList.fromTable {"bar"}
+dll:addAt(2, "foo")
+assertEquals(dll:tostring(), "[bar, foo]", "2.1.3.0")
+assertEquals(dll:tostringTailToHead(), "[foo, bar]", "2.1.3.1")
+
+-- Test 2.1.4
+-- `DoublyLinkedList#addAt(5)` with list having one value.
+testcase = "2.1.4.0"
+dll = DoublyLinkedList.fromTable {"foo"}
+xpcall (function() dll:addAt(5, "bar") end, addError) 
+
+-- Test 2.1.5
+-- `DoublyLinkedList#addAt(2)` with list having three values.
+dll = DoublyLinkedList.fromTable {"foo", "bar", "baz"}
+dll:addAt(2, "qbz")
+assertEquals(dll:tostring(), "[foo, qbz, bar, baz]", "2.1.5.0")
+assertEquals(dll:tostringTailToHead(), "[baz, bar, qbz, foo]", "2.1.5.1")
+
+-- Test 2.1.6
+-- `DoublyLinkedList#addAt(4)` with list having three values.
+dll = DoublyLinkedList.fromTable {"foo", "bar", "baz"}
+dll:addAt(4, "qbz")
+assertEquals(dll:tostring(), "[foo, bar, baz, qbz]", "2.1.6.0")
+assertEquals(dll:tostringTailToHead(), "[qbz, baz, bar, foo]", "2.1.6.1")
+
+
+-- Test 2.1.7
+-- `DoublyLinkedList#addAt(5)` with list having three values.
+testcase = "2.1.7.0"
+dll = DoublyLinkedList.fromTable {"foo", "bar", "baz"}
+xpcall (function() dll:addAt(5, "qbz") end, addError) 
+
+-- Test 2.1.8
+-- `DoublyLinkedList#addAt(-1)` with list.
+dll = DoublyLinkedList.fromTable {"foo"}
+xpcall(function()
+		dll:addAt(-1, "bar")
+	end,
+	
+	function(err)
+		if (err:find("failed!")) then
+			print(err)
+		else
+			err = err:sub(err:find('\027%[31m') + 5, #err - 4) -- Picking out the message and removing the control characters.
+			assertEquals(err, "Index (-1) is out of bounds.", "2.1.8.0")
+		end
+	end
+)
+
+testcase = nil
+
+-- Test 2.2.0
+-- `DoublyLinkedList#addAll` empty array inside empty list.
+dll = DoublyLinkedList.fromTable {}
+dll:addAll {}
+assertEquals(dll:tostring(), "[]", "2.2.0.0")
+assertEquals(dll:tostringTailToHead(), "[]", "2.2.0.1")
+
